9e4c6bcb20e5355911af54ec1a39e6c00fcd8d74
andresnsn <59550765+andresnsn@users.noreply.github.com>
2021-03-03 18:49:58 -0300

    Adicionando comentários

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     /*
     Aqui no método main, eu deixei apenas um Singleton que inicia a aplicação. Este Singleton está no pacote "appstart".
     A aplicação funciona de maneira retilínea, e assim que é terminado o seu percurso lógico, encerra e descarta as informações
-    de tempo de execução, já que a ideia é focar na implementação da orientação a objetos.
+    de tempo de execução, já que a ideia é focar na implementação da orientação a objetos. Também não possui tratativas de erro.
      */
 
     public static void main(String[] args) {
